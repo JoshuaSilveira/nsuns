54590c618556dd477d1ee625147c86657650ec62
josh <joshua.silveira92@gmail.com>
2019-08-25 13:16:52 -0400

    Updates

---
+++ precache-manifest.b78507b4448084965244b19b8b1750d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78b92bc8a81f1fe73589f41992d2a1c",
+    "url": "/nsuns/index.html"
+  },
+  {
+    "revision": "a2af01f03a932f426d00",
+    "url": "/nsuns/static/css/main.1fce661d.chunk.css"
+  },
+  {
+    "revision": "30a380c6b80d9161473a",
+    "url": "/nsuns/static/js/2.af0cd1c8.chunk.js"
+  },
+  {
+    "revision": "a2af01f03a932f426d00",
+    "url": "/nsuns/static/js/main.04dd0b82.chunk.js"
+  },
+  {
+    "revision": "6826867ea8ed8a72fc42",
+    "url": "/nsuns/static/js/runtime~main.a63ebaa3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nsuns/static/media/logo.5d5d9eef.svg"
+  }
+]);
